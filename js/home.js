f01d03c44bac7641e9288c019ccb58b0b48a4ab4
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-05-27 14:38:39 +0600

    Add Money

--- js/home.js
+++ js/home.js
@@ -1,11 +1,21 @@
 // Home Screen
-let addAmountText = document.getElementById("addAmountText");
+let addAmountInput = document.getElementById("addAmountInput");
+let pinInput = document.getElementById("pinInput");
+let pinText = document.getElementById("pinText");
 let availableBalance = document.getElementById("availableBalance");
 let moneyBtn = document.getElementById("moneyBtn");
 
 moneyBtn.addEventListener("click", function () {
-  if (addAmountText.value == "" || pinText.value == "") {
+  if (addAmountInput.value == "" || pinInput.value == "") {
     alert("Credentials cannot be empty.");
+  } else if (addAmountInput > "0" && pinInput.value == "1234") {
+    alert("Amount added successfully!");
+    let currentBalance = parseFloat(availableBalance.innerHTML);
+    let newBalance = parseFloat(addAmountInput.value);
+    availableBalance.innerText = (currentBalance + newBalance);
+
+  } else {
+    alert("Wrong credentials! Please try again.");
   }
 });
 // Home Screen
