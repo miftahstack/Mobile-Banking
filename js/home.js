4ac6b9a4b585f921f021e22f1fb7e5831f0fdf7d
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-05-30 19:42:15 +0600

    Added btn

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
+// Home Screen
 let pinInput = document.getElementById("pinInput");
 let pinText = document.getElementById("pinText");
 let moneyBtn = document.getElementById("moneyBtn");
 
-// Home Screen
 let addAmountInput = document.getElementById("addAmountInput");
 let availableBalance = document.getElementById("availableBalance");
 let hidebutton = document.getElementById("hidebutton");
@@ -12,34 +12,33 @@ let cashOutInput = document.getElementById("cashOutInput");
 
 // Add Money
 moneyBtn.addEventListener("click", function () {
-    if (addAmountInput.value == "" || pinInput.value == "") {
-        alert("Credentials cannot be empty.");
-    } else if (addAmountInput > "0" && pinInput.value == "1234") {
-        alert("Amount added successfully!");
-        let currentBalance = parseFloat(availableBalance.innerHTML);
-        let newBalance = parseFloat(addAmountInput.value);
-        availableBalance.innerText = currentBalance + newBalance;
-    } else {
-        alert("Wrong credentials! Please try again.");
-    }
+  if (addAmountInput.value == "" || pinInput.value == "") {
+    alert("Credentials cannot be empty.");
+  } else if (addAmountInput > "0" && pinInput.value == "1234") {
+    alert("Amount added successfully!");
+    let currentBalance = parseFloat(availableBalance.innerHTML);
+    let newBalance = parseFloat(addAmountInput.value);
+    availableBalance.innerText = currentBalance + newBalance;
+  } else {
+    alert("Wrong credentials! Please try again.");
+  }
 });
-// Add Money
+// Add Money____
 
-// CashOut 
+// CashOut
+let addMoneyBtn = document.getElementById("addMoneyBtn");
+let addMoney = document.getElementById("addMoney");
+let cashOutBtn = document.getElementById("cashOutBtn");
+let cashOutMoney = document.getElementById("cashOutMoney");
 
-
-// CashOut 
-
-
+cashOutBtn.addEventListener("click", () => {
+  addMoney.classList.add("hidden");
+  cashOutMoney.classList.remove("hidden");
+});
+addMoneyBtn.addEventListener("click", () => {
+  cashOutMoney.classList.add("hidden");
+  addMoney.classList.remove("hidden");
+});
+// CashOut
 
 // Home Screen
-
-let addMoneyBtn= document.getElementById("addMoneyBtn")
-let addMoney= document.getElementById("addMoney")
-let cashOutBtn= document.getElementById("cashOutBtn")
-
-cashOutBtn.addEventListener("click",()=>{
-    addMoney.classList.add("hidden");
-    cashOutMoney.classList.remove("hidden");
-
-})
