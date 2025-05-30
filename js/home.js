08a79e3dc33253d73000629b71e01a4778d88465
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-05-30 19:20:43 +0600

    Added buttons

--- js/home.js
+++ js/home.js
@@ -1,25 +1,45 @@
-// Home Screen
-let addAmountInput = document.getElementById("addAmountInput");
 let pinInput = document.getElementById("pinInput");
 let pinText = document.getElementById("pinText");
-let availableBalance = document.getElementById("availableBalance");
 let moneyBtn = document.getElementById("moneyBtn");
 
+// Home Screen
+let addAmountInput = document.getElementById("addAmountInput");
+let availableBalance = document.getElementById("availableBalance");
+let hidebutton = document.getElementById("hidebutton");
+let cardMoney = document.getElementById("cardMoney");
+// =============================================================
+let cashOutInput = document.getElementById("cashOutInput");
+
+// Add Money
 moneyBtn.addEventListener("click", function () {
-  if (addAmountInput.value == "" || pinInput.value == "") {
-    alert("Credentials cannot be empty.");
-  } else if (addAmountInput > "0" && pinInput.value == "1234") {
-    alert("Amount added successfully!");
-    let currentBalance = parseFloat(availableBalance.innerHTML);
-    let newBalance = parseFloat(addAmountInput.value);
-    availableBalance.innerText = currentBalance + newBalance;
-  } else {
-    alert("Wrong credentials! Please try again.");
-  }
+    if (addAmountInput.value == "" || pinInput.value == "") {
+        alert("Credentials cannot be empty.");
+    } else if (addAmountInput > "0" && pinInput.value == "1234") {
+        alert("Amount added successfully!");
+        let currentBalance = parseFloat(availableBalance.innerHTML);
+        let newBalance = parseFloat(addAmountInput.value);
+        availableBalance.innerText = currentBalance + newBalance;
+    } else {
+        alert("Wrong credentials! Please try again.");
+    }
 });
+// Add Money
+
+// CashOut 
 
 
+// CashOut 
 
 
 
 // Home Screen
+
+let addMoneyBtn= document.getElementById("addMoneyBtn")
+let addMoney= document.getElementById("addMoney")
+let cashOutBtn= document.getElementById("cashOutBtn")
+
+cashOutBtn.addEventListener("click",()=>{
+    addMoney.classList.add("hidden");
+    cashOutMoney.classList.remove("hidden");
+
+})
